d9858685b1d09f82c8baa74a6fff00f99fa46e5d
Julien Biezemans <jb@jbpros.com>
2025-05-25 23:02:08 +0200

    Emerge from nothing

---
+++ lua/community.lua
@@ -0,0 +1,22 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.elixir-phoenix" },
+  { import = "astrocommunity.pack.helm" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.python" },
+  -- { import = "astrocommunity.pack.ruby" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.terraform" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.xml" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- import/override with your plugins folder
+}
